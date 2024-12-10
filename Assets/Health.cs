8f7342a84882fa2249c56da5689aa541d708552c
reissnja <jannis.reissner@web.de>
2024-12-10 10:49:34 +0100

    Implemented 4 - 6

---
+++ Assets/Health.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Search;
+
+public class Health : MonoBehaviour
+{
+    [Header("Attributes")] 
+    [SerializeField] private int hitPoints = 2;
+
+    private bool isDestroyed = false;
+
+    public void TakeDamage(int dmg)
+    {
+        hitPoints -= dmg;
+
+        if (hitPoints <= 0 && !isDestroyed)
+        {
+            EnemySpawner.onEnemyDestroy.Invoke();
+            isDestroyed = true;
+            Destroy(gameObject);
+        }
+    }
+    
+}
