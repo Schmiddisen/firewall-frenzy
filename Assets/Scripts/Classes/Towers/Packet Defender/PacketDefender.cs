10bc158e9243bb52ad488cad864d6adf2a055837
unknown <jannis.reissner@telekom.de>
2025-01-27 11:21:35 +0100

    [Feature] Finished Packet Defender Upgrade Paths

--- Assets/Scripts/Classes/Towers/Packet Defender/PacketDefender.cs
+++ Assets/Scripts/Classes/Towers/Packet Defender/PacketDefender.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PacketDefender : TargetingTower
 {
     [Header("Upgrade JSON")]
     [SerializeField] TextAsset upgradeJson;
 
+    [Header("Projectiles")]
+    [SerializeField] GameObject knockbackBullet;
+    [SerializeField] GameObject laserBullet;
+    [SerializeField] LineRenderer laserLR;
+ 
     TowerPathUpgrades upgradeData;
 
     
@@ -12,10 +18,18 @@ public class PacketDefender : TargetingTower
         this.upgradeData = JsonUtility.FromJson<TowerPathUpgrades>(upgradeJson.text);
     }
     
-    public override void Shoot()
-    {
-        base.Shoot();
-        Debug.Log("Override Shoot");
+    public override void updateMethod() {
+        //Make a Copy because maybe an Enemy will exit the range right in the time when we iterate the list
+        List<Transform> targetsCopy = new List<Transform>(base.enemyTargets);
+        currentTarget = TargetingCalculator.getTargetAfterPriority(this.targetPrio, targetsCopy);
+
+        if (currentTarget != null) {
+            base.RotateTowardsTarget();
+        }
+        //Draw Laser if PathA maxlevel
+        if (base.upgradePath == UpgradePath.PathA && base.currentLevel == 3) {
+            drawLaser();
+        }
     }
 
     public override void upgrade(UpgradePath path)
@@ -24,7 +38,23 @@ public class PacketDefender : TargetingTower
         Upgrades upgradeData = path == UpgradePath.PathA ? this.upgradeData.PathA : this.upgradeData.PathB;
 
         base.applyUpgrade(upgradeData, path);
+
+        if (base.upgradePath == UpgradePath.PathA && base.currentLevel == 3) {
+            base.bulletPrefab = this.laserBullet;
+        }
+
+        if (base.upgradePath == UpgradePath.PathB && base.currentLevel == 3) {
+            base.bulletPrefab = this.knockbackBullet;
+        }
         
     }
-
+    private void drawLaser() {
+        if (currentTarget == null) {
+            laserLR.enabled = false;
+            return;
+        }
+        laserLR.enabled = true;
+        laserLR.SetPosition(0, towerFiringPoint.position);
+        laserLR.SetPosition(1, currentTarget.position);
+    }
 }
