aaf756229d33a7b36a515e52dab6ffa03bffc3b6
JannisProv <jannis.reissner@stud-provadis-hochschule.de>
2025-02-09 18:11:19 +0100

    [Feature] Money will now be used, when building or upgrading towers. Added cost to the upgrade.json. Disabled the upgrade Buttons when placing tower. Added the ability to right click the mosue in order to stop tower placement. Added an info Text prefab and Info text spawner, in order to give feedback to the player if something went wront

---
+++ Assets/Scripts/Helpers/FloatingTextSpawner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FloatingTextSpawner : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject floatingTextPrefab;
+    [SerializeField]
+    public Canvas canvas;
+
+    public static FloatingTextSpawner main;
+
+    public void Awake()
+    {
+        main = this;
+    }
+
+    public void spawnFloatingText(string message, Vector2 position)
+    {
+        Debug.Log("spawn Floating text");
+        GameObject instance = Instantiate(floatingTextPrefab, canvas.transform);
+        
+        RectTransform rectTransform = instance.GetComponent<RectTransform>();
+        rectTransform.position = position;
+
+        FloatingText floatingText = instance.GetComponent<FloatingText>();
+        floatingText.SetText(message);
+    }
+}
